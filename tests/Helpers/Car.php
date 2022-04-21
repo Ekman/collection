0d7c0ed6e85bc26823420830daa183755e51e8a1
Niklas Ekman <niklas@stockfiller.com>
2022-04-21 14:49:14 +0200

    Properly implement CircleCI (#3)
    * fix
    
    * ping
    
    * php 8 fixes
    
    * ping

--- tests/Helpers/Car.php
+++ tests/Helpers/Car.php
@@ -4,7 +4,7 @@ namespace Nekman\Collection\Tests\Helpers;
 
 class Car extends Machine
 {
-    public function __construct($name, private readonly int $numberOfSeats)
+    public function __construct($name, private int $numberOfSeats)
     {
         parent::__construct($name);
     }
